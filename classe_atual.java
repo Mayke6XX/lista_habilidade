a087a165faff9036ce59445b0040ec8bb449fbc1
Mayke <81714707+Mayke6XX@users.noreply.github.com>
2021-03-31 18:24:13 -0300

    Create classe_atual.java

---
+++ classe_atual.java
@@ -0,0 +1,23 @@
+package br.com.sample.domain.orm;
+ 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+ 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+ 
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+ 
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String nome;
+    private String status;
+    private Integer rank;
+}
